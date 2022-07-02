52263f021fa9647e9c8bf2ab36b90e7ae35e682c
Claire C <claire.chabas@gmail.com>
2022-07-02 19:04:41 +0100

    chore(config): adding basePath to next config to fix links between pages + adding explanatory comment

--- next.config.js
+++ next.config.js
@@ -4,5 +4,9 @@ module.exports = {
   nextConfig: {
     reactStrictMode: true,
   },
+  // Since this app is server on a Github pages subpath
+  // we need to specify this subpath so the app can access
+  // css and assets and for links between pages to work
   assetPrefix: '/full-stack-nextjs-grocery-list/',
+  basePath: '/full-stack-nextjs-grocery-list/',
 };
