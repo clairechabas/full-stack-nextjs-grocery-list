1f1629d0f2da1fb25334e4be828fde2fad223b15
Claire C <claire.chabas@gmail.com>
2022-07-02 16:31:57 +0100

    feat(front): creating list related components

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -50,3 +50,30 @@ export const CardHeader: NextPage<CardHeaderProps> = ({
     </div>
   )
 }
+
+export const List: NextPage<CardProps> = ({ children }) => {
+  return (
+    <div className='overflow-y-auto h-72'>{ children }</div>
+  )
+}
+
+interface ListItemProps {
+  item: GroceryList
+  onUpdate?: (item: GroceryList) => void
+}
+
+export const ListItemComponent: NextPage<ListItemProps> = ({ item, onUpdate }) => {
+  return (
+    <div className='h-12 border-b flex items-center justify-start px-3'>
+      <input 
+        type="checkbox"
+        className='w-4 h-4 border-gray-300 rounded mr-4'
+        defaultChecked={item.checked as boolean}
+        onChange={() => onUpdate?.(item)}
+      />
+      <h2 className='text-gray-600 tracking-wide text-sm'>{ item.title }</h2>
+    </div>
+  )
+}
+
+export const ListItem = memo(ListItemComponent)
