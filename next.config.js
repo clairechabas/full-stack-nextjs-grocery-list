aec411ea168fd90a89bfdae916eacf015dc40db5
Claire C <claire.chabas@gmail.com>
2022-07-02 19:08:15 +0100

    chore(config): fixing next config format

--- next.config.js
+++ next.config.js
@@ -3,10 +3,10 @@
 module.exports = {
   nextConfig: {
     reactStrictMode: true,
+    // Since this app is server on a Github pages subpath
+    // we need to specify this subpath so the app can access
+    // css and assets and for links between pages to work
+    assetPrefix: '/full-stack-nextjs-grocery-list/',
+    basePath: '/full-stack-nextjs-grocery-list/',
   },
-  // Since this app is server on a Github pages subpath
-  // we need to specify this subpath so the app can access
-  // css and assets and for links between pages to work
-  assetPrefix: '/full-stack-nextjs-grocery-list/',
-  basePath: '/full-stack-nextjs-grocery-list/',
 };
