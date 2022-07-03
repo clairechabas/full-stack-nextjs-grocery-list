27e06feb753936588e2de61365efff9aa0e70ac7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2022-07-03 10:02:23 +0000

    Deploy clairechabas/full-stack-nextjs-grocery-list to github.com/clairechabas/full-stack-nextjs-grocery-list.git:gh-pages

---
+++ _next/static/FXHudeCGKcHoeDGX-mu_z/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-66a46d20c93f3644.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
