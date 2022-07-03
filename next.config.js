972a198faa58dfdef6627dd8c72b0183e3be15f1
Claire C <claire.chabas@gmail.com>
2022-07-03 11:00:14 +0100

    chore(config): updating next config and adding nojekyll files to bypass Jekyll processing on GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 
-module.exports = {
-  nextConfig: {
-    reactStrictMode: true,
-    // Since this app is server on a Github pages subpath
-    // we need to specify this subpath so the app can access
-    // css and assets and for links between pages to work
-    assetPrefix: '/full-stack-nextjs-grocery-list/',
-    basePath: '/full-stack-nextjs-grocery-list/',
-  },
+const nextConfig = {
+  reactStrictMode: true,
+  // Since this app is server on a Github pages subpath
+  // we need to specify this subpath so the app can access
+  // css and assets and for links between pages to work
+  assetPrefix: '/full-stack-nextjs-grocery-list/',
+  basePath: '/full-stack-nextjs-grocery-list',
 };
+
+module.exports = nextConfig;
